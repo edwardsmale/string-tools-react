7f328a6d2dc745893a353936b00211012ec7de0b
Edward Smale <smaleedward@gmail.com>
2021-01-23 19:12:58 +0000

    Catch exceptions in command processing

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -13,112 +13,96 @@ export class CommandService {
     }
 
     processCommands(codeValue: string, inputValue: string, explain: boolean): string[][] {
-        let codeLines = this.textUtilsService.TextToLines(codeValue);
-        let lines = this.textUtilsService.TextToLines(inputValue);
 
-        let context: Context = {
-            isTabDelimited: this.textUtilsService.IsTabDelimited(lines),
-            regex: null,
-            searchString: null,
-            isColumnNumeric: null
-        };
+        try {
+            let codeLines = this.textUtilsService.TextToLines(codeValue);
+            let lines = this.textUtilsService.TextToLines(inputValue);
 
-        let currentValues: (string | string[])[] = lines;
+            let context: Context = {
+                isTabDelimited: this.textUtilsService.IsTabDelimited(lines),
+                regex: null,
+                searchString: null,
+                isColumnNumeric: null
+            };
 
-        for (let i = 0; i < codeLines.length; i++) {
+            let currentValues: (string | string[])[] = lines;
 
-            let parsedCommand = this.commandParsingService.ParseCodeLine(
-                codeLines[i]
-            );
+            for (let i = 0; i < codeLines.length; i++) {
 
-            let newValues: (string | string[])[] = [];
+                let parsedCommand = this.commandParsingService.ParseCodeLine(
+                    codeLines[i]
+                );
 
-            if (parsedCommand.commandType.name === "flat") {
+                let newValues: (string | string[])[] = [];
 
-                if (!parsedCommand.para || !this.textUtilsService.IsPositiveInteger(parsedCommand.para)) {
-                    let flattened: string[] = [];
+                if (parsedCommand.commandType.name === "flat") {
 
-                    for (let j = 0; j < currentValues.length; j++) {
+                    if (!parsedCommand.para || !this.textUtilsService.IsPositiveInteger(parsedCommand.para)) {
+                        let flattened: string[] = [];
 
-                        if (Array.isArray(currentValues[j])) {
+                        for (let j = 0; j < currentValues.length; j++) {
 
-                            for (let k = 0; k < (currentValues[j] as string[]).length; k++) {
-                                flattened.push(currentValues[j][k]);
+                            if (Array.isArray(currentValues[j])) {
+
+                                for (let k = 0; k < (currentValues[j] as string[]).length; k++) {
+                                    flattened.push(currentValues[j][k]);
+                                }
+
+                            } else {
+                                flattened.push(currentValues[j] as string);
                             }
-
-                        } else {
-                            flattened.push(currentValues[j] as string);
                         }
-                    }
 
-                    newValues[0] = flattened;
-                } else {
-                    let batchSize = parseInt(parsedCommand.para, 10);
-                    let batches = [];
+                        newValues[0] = flattened;
+                    } else {
+                        let batchSize = parseInt(parsedCommand.para, 10);
+                        let batches = [];
 
-                    let flattened: string[] = [];
+                        let flattened: string[] = [];
 
-                    for (let j = 0; j < currentValues.length; j++) {
+                        for (let j = 0; j < currentValues.length; j++) {
 
-                        if (Array.isArray(currentValues[j])) {
+                            if (Array.isArray(currentValues[j])) {
 
-                            for (let k = 0; k < (currentValues[j] as string[]).length; k++) {
-                                flattened.push(currentValues[j][k]);
+                                for (let k = 0; k < (currentValues[j] as string[]).length; k++) {
+                                    flattened.push(currentValues[j][k]);
+
+                                    if (flattened.length === batchSize) {
+                                        batches.push(flattened);
+                                        flattened = [];
+                                    }
+                                }
+
+                            } else {
+                                flattened.push(currentValues[j] as string);
 
                                 if (flattened.length === batchSize) {
                                     batches.push(flattened);
                                     flattened = [];
                                 }
                             }
-
-                        } else {
-                            flattened.push(currentValues[j] as string);
-
-                            if (flattened.length === batchSize) {
-                                batches.push(flattened);
-                                flattened = [];
-                            }
                         }
+
+                        if (flattened.length) {
+                            batches.push(flattened);
+                        }
+
+                        newValues = batches;
                     }
-
-                    if (flattened.length) {
-                        batches.push(flattened);
-                    }
-
-                    newValues = batches;
-                }
-            } else {
-
-                // Not flat|batch command.
-
-                if (parsedCommand.commandType.name === "split") {
-                    context.isColumnNumeric = [] as boolean[];
-                }
-
-                // Iterate through the lines and apply the command.
-
-                if ((parsedCommand.commandType.isArrayBased && !Array.isArray(currentValues[0])) || parsedCommand.commandType.name === "sort") {
-
-                    const newLineValue = parsedCommand.commandType.exec(
-                        currentValues as string[],
-                        parsedCommand.para,
-                        parsedCommand.negated,
-                        context,
-                        explain
-                    );
-
-                    if (newLineValue !== null) {
-                        newValues = newLineValue as string[];
-                    }
-
                 } else {
 
-                    let commandType = parsedCommand.commandType as CommandType;
+                    // Not flat|batch command.
 
-                    for (let j = 0; j < currentValues.length; j++) {
+                    if (parsedCommand.commandType.name === "split") {
+                        context.isColumnNumeric = [] as boolean[];
+                    }
 
-                        const newLineValue = commandType.exec(
-                            currentValues[j] as string,
+                    // Iterate through the lines and apply the command.
+
+                    if ((parsedCommand.commandType.isArrayBased && !Array.isArray(currentValues[0])) || parsedCommand.commandType.name === "sort") {
+
+                        const newLineValue = parsedCommand.commandType.exec(
+                            currentValues as string[],
                             parsedCommand.para,
                             parsedCommand.negated,
                             context,
@@ -126,40 +110,65 @@ export class CommandService {
                         );
 
                         if (newLineValue !== null) {
-                            newValues.push(newLineValue as string);
+                            newValues = newLineValue as string[];
+                        }
+
+                    } else {
+
+                        let commandType = parsedCommand.commandType as CommandType;
+
+                        for (let j = 0; j < currentValues.length; j++) {
+
+                            const newLineValue = commandType.exec(
+                                currentValues[j] as string,
+                                parsedCommand.para,
+                                parsedCommand.negated,
+                                context,
+                                explain
+                            );
+
+                            if (newLineValue !== null) {
+                                newValues.push(newLineValue as string);
+                            }
                         }
                     }
                 }
+
+                currentValues = newValues;
             }
 
-            currentValues = newValues;
-        }
+            let output: string[][] = [];
 
-        let output: string[][] = [];
+            if (explain) {
 
-        if (explain) {
-
-            for (let i = 0; i < codeLines.length; i++) {
-                let parsedCommand = this.commandParsingService.ParseCodeLine(codeLines[i]);
-                let para = parsedCommand.para;
-                let negated = parsedCommand.negated;
-                let explanation = parsedCommand.commandType.exec(lines, para, negated, context, true) as Explanation;
-                output.push([explanation.explanation]);
-            }
-
-            return output;
-
-        } else {
-
-            for (let i = 0; i < currentValues.length; i++) {
-                var value = currentValues[i];
-                if (Array.isArray(value)) {
-                    var arrayValue = value as string[];
-                    output.push(arrayValue);
-                } else {
-                    output.push([value as string]);
+                for (let i = 0; i < codeLines.length; i++) {
+                    let parsedCommand = this.commandParsingService.ParseCodeLine(codeLines[i]);
+                    let para = parsedCommand.para;
+                    let negated = parsedCommand.negated;
+                    let explanation = parsedCommand.commandType.exec(lines, para, negated, context, true) as Explanation;
+                    output.push([explanation.explanation]);
                 }
+
+                return output;
+
+            } else {
+
+                for (let i = 0; i < currentValues.length; i++) {
+                    var value = currentValues[i];
+                    if (Array.isArray(value)) {
+                        var arrayValue = value as string[];
+                        output.push(arrayValue);
+                    } else {
+                        output.push([value as string]);
+                    }
+                }
+
+                return output;
             }
+        } catch (ex) {
+
+            let output:string[][] = [];
+            output.push([ex.toString()]);
 
             return output;
         }
